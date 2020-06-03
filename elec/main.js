d8d5bfb6c24a2d1276f11be915a028e5069ead84
Harkame <louisr.daviaud@gmail.com>
2020-06-03 13:54:13 +0200

    dev

--- elec/main.js
+++ elec/main.js
@@ -2,8 +2,11 @@ const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const ipc = electron.ipcMain
-let $ = require('jquery');
+
 let mainWindow;
+const url = require('url')
+const path = require('path')
+const electronLocalshortcut = require('electron-localshortcut');
 
 function createWindow ()
 {
@@ -13,23 +16,49 @@ function createWindow ()
     title: 'ZdS, ça pulpe !',
     maximized: false,
     center: true,
+    webPreferences:
+    {
+      nodeIntegration: true,
+      //devTools: false
+    },
 });
   //mainWindow.maximize();
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  //mainWindow.loadURL(`file://${__dirname}/index.html`);
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: 'file',
+    slashes: true
+  }))
+  electronLocalshortcut.register(mainWindow, 'F12', () => {
+    mainWindow.openDevTools();
+  });
+
+  mainWindow.webContents.on('did-finish-load', function()
+  {
+    mainWindow.openDevTools();
+    /*
+    document.querySelector('#btn').addEventListener('click', () => {
+        getData()
+    })
+    */
+  });
 
   mainWindow.on('closed', () =>
   {
     mainWindow = null;
   })
+
+  //mainWindow.webContents.on("devtools-opened", () => { mainWindow.webContents.closeDevTools(); });
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', () =>
-{
-  app.quit();
-});
+app.on('window-all-closed', () => {
+  if(process.platform !== 'Darwin') {
+    app.quit()
+  }
+})
 
 app.on('activate', () =>
 {
@@ -37,7 +66,22 @@ app.on('activate', () =>
     createWindow()
 });
 
-ipc.on('message-synchrone', function (event, arg) {
+app.on('active', () => {
+  if(mainWindow == null) {
+    createWindow()
+  }
+})
+
+ipc.on('synchronous-message', function (event, arg) {
+  if(arg.type === "ping") {
+    console.log("Received pong successfully!")
+    event.returnValue = "receivpong";
+  }
+  else
+    event.returnValue = "reeived";
+})
+
+ipc.on('asynchronous-message', function (event, arg) {
   console.log(arg)
   event.returnValue = 'Oui voilà je réponds !'
 })
--- elec/myscript.js
+++ elec/myscript.js
@@ -1,8 +1,56 @@
 const ipc = require('electron').ipcRenderer
+let $ = require('jquery');
+//require('./models/model.js')
+/*
+let notifyBtn = document.getElementById("btn");
 
-const btn = document.getElementById('btn')
-btn.addEventListener('click', function () {
-  console.log("sendsync");
-    const reponse = ipc.sendSync('message-synchrone', 'Allez hop !')
-    document.getElementById('syncReponse').innerHTML = reponse
+notifyBtn.addEventListener('click', function(event)
+{
+  ipcRenderer.send('asynchronous-message', 'ping')
 })
+*/
+
+$('#btn').on("click", function(event)
+{
+  //ipcRenderer.send('asynchronous-message', 'ping');
+  //const reponse = ipc.sendSync("synchronous-message", { type: "ping"});
+  //console.log(reponse);
+  let models = [];
+  models.push(new Model())
+  models.push(new Model())
+  models.push(new Model())
+  models.push(new Model())
+  models.push(new Model())
+
+  $('#models').empty();
+
+  models.forEach(function(model)
+  {
+    $('#models').append(`
+      <li class="list-group-item">
+        <p>${model.name}</p>
+      </li>
+    `);
+  });
+});
+ipc.on('asynchronous-message', (event, arg) =>
+{
+  console.log(arg);
+})
+
+var index = 0;
+class Model {
+  constructor() {
+    this.id = 'id_' + index++;
+    this.name = "ModelName-" + index;
+  }
+  set name(name) {
+    this._name = name.charAt(0).toUpperCase() + name.slice(1);
+  }
+  get name() {
+    return this._name;
+  }
+  sayHello() {
+    console.log('Hello, my name is ' + this.name + ', I have ID: ' + this.id);
+  }
+}
